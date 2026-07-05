f796723509b072f8bc56d3e8145202ec79a93422
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-07-05 23:14:23 +0530

    Time: 0 ms (100%), Space: 13.3 MB (66.21%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count=0;
+        for(auto x:nums){
+            int digitcount=0;
+            while(x>0){
+                digitcount++;
+                x/=10;
+
+             }
+             if(digitcount&1){
+                continue;
+             }
+                else{
+                    count++;
+                }
+            }
+            return count;
+        }
+    
+};
