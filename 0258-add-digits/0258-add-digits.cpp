c420396de04934a79ec31cc2423425d53c7a1458
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-07-10 23:25:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.82%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>9)
+      {
+       int ans=0,rem;
+       while(num){
+        rem=num%10;
+        num=num/10;
+        ans=rem+ans;
+       } 
+       num=ans;
+     }
+       return num;
+     
+    }
+};
