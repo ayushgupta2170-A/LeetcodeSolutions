dfbf0404900cd0d59b468e448d8638508b3bf9e6
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-07-12 22:39:50 +0530

    Time: 0 ms (100%), Space: 11.9 MB (39.69%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countStudents(vector<int>& students,vector<int>& sandwiches) {
+        queue<int> q;
+
+        for(int x:students)
+            q.push(x);
+
+        int i=0,cnt=0;
+
+        while(!q.empty()&&cnt<q.size()){
+            if(q.front()==sandwiches[i]){
+                q.pop();
+                i++;
+                cnt=0;
+            }
+            else{
+                q.push(q.front());
+                q.pop();
+                cnt++;
+            }
+        }
+
+        return q.size();
+    }
+};
