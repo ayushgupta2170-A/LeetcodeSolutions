dc5bfffd82adfeadd1e19e4cb800b15d1d9981ae
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-08-12 22:31:55 +0530

    Time: 78 ms (19.89%), Space: 149.4 MB (56.74%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,25 +4,17 @@ public:
         int ans=0;
         int n=nums.size();
         unordered_map<int,int>mp;
-        int i=0,j=0,culprit=0;
+        int i=0,j=0;
         while(j<n){
             mp[nums[j]]++;
-            if(mp[nums[j]]==k+1){
-                culprit++;
-            }
-            if(culprit>0){
-                mp[nums[i]]--;
-                if(mp[nums[i]]==k){
-                    culprit--;
-                }
-                i++;
-            }
-            if(culprit==0){
-                ans=max(ans,j-i+1);
-            }
 
+            while(i<j && mp[nums[j]]>k){
+                mp[nums[i]]--;
+                i++;
+
+            }
+            ans=max(ans,j-i+1);
             j++;
-            
         }
         return ans;
     }
